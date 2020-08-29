0cf515c57e76d5c26aac206354fe2aa3f1df8fb6
cezklosowski <cez.klosowski@gmail.com>
2020-08-29 11:32:43 +0200

    Connection to database

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa.hibernate");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+//        saveEmployee(entityManager);
+//        getEmployee(entityManager);
+//        updateEmployee(entityManager);
+//        refreshEmployee(entityManager);
+//        deleteEmployee(entityManager);
+//        detachAndMergeEmployee(entityManager);
+
+        entityManager.close();
+        entityManagerFactory.close();
+
     }
 }
